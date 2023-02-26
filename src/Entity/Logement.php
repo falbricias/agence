d9b3dacfa4a2b9acff3571abe6726c0634296fe3
falbricias2022 <francois.albricias2022@campus-eni.fr>
2023-02-26 12:43:29 +0100

    MAJ agence

---
+++ src/Entity/Logement.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LogementRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LogementRepository::class)]
+class Logement
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?float $superficie = null;
+
+    #[ORM\Column]
+    private ?int $nbPieces = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $type = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $adresse = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $piscine = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $surfaceExterieure = null;
+
+    #[ORM\Column]
+    private ?bool $garage = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $modalite = null;
+
+    #[ORM\Column]
+    private ?float $prix = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dateParution = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSuperficie(): ?float
+    {
+        return $this->superficie;
+    }
+
+    public function setSuperficie(float $superficie): self
+    {
+        $this->superficie = $superficie;
+
+        return $this;
+    }
+
+    public function getNbPieces(): ?int
+    {
+        return $this->nbPieces;
+    }
+
+    public function setNbPieces(int $nbPieces): self
+    {
+        $this->nbPieces = $nbPieces;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function isPiscine(): ?bool
+    {
+        return $this->piscine;
+    }
+
+    public function setPiscine(?bool $piscine): self
+    {
+        $this->piscine = $piscine;
+
+        return $this;
+    }
+
+    public function getSurfaceExterieure(): ?int
+    {
+        return $this->surfaceExterieure;
+    }
+
+    public function setSurfaceExterieure(?int $surfaceExterieure): self
+    {
+        $this->surfaceExterieure = $surfaceExterieure;
+
+        return $this;
+    }
+
+    public function isGarage(): ?bool
+    {
+        return $this->garage;
+    }
+
+    public function setGarage(bool $garage): self
+    {
+        $this->garage = $garage;
+
+        return $this;
+    }
+
+    public function getModalite(): ?string
+    {
+        return $this->modalite;
+    }
+
+    public function setModalite(string $modalite): self
+    {
+        $this->modalite = $modalite;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getDateParution(): ?\DateTimeInterface
+    {
+        return $this->dateParution;
+    }
+
+    public function setDateParution(\DateTimeInterface $dateParution): self
+    {
+        $this->dateParution = $dateParution;
+
+        return $this;
+    }
+}
